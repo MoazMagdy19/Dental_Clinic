b96d0eb3bd2451e58f88df6cc50a02d5deccbb09
Moaz Magdy <128479222+MoazMagdy19@users.noreply.github.com>
2025-08-31 17:24:08 +0300

    Update script.js

--- script.js
+++ script.js
@@ -65,6 +65,7 @@ form?.addEventListener('submit', (e) => {
 الحالة: ${data.status === "new" ? "كشف جديد" : "متابعة"}
 الخدمة: ${data.status === "new" ? "-" : data.service}
 التاريخ: ${data.date}
+الساعة: ${data.time}
 ملاحظات: ${data.notes || '-'} `;
 
   summary.textContent = msg;
